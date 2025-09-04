d5740d1ed59c5492c30b8b9a9ad4423bb5a3d012
AleksZhd <az@az.az>
2025-09-04 18:11:53 +0000

    modified:   flaskapp/ovpn/ovpn_server.py 	modified:   flaskapp/ovpn/ovpn_server_lib.py 	modified:   flaskapp/temp/netstat_info 	modified:   flaskapp/temp/network_info 	modified:   flaskapp/temp/ovpn_server.status 	modified:   flaskapp/temp/ovpn_tree 	modified:   flaskapp/temp/server.conf 	new file:   flaskapp/temp/server.new 	modified:   flaskapp/temp/server.tmp 	modified:   flaskapp/temp/system_info 	modified:   flaskapp/templates/base.html 	modified:   flaskapp/templates/ovpn/ovpn_server.html 	new file:   flaskapp/templates/ovpn/ovpn_server_logs.html 	new file:   flaskapp/templates/ovpn/server_db_setup.html 	modified:   instance/flask.db

--- flaskapp/ovpn/ovpn_server.py
+++ flaskapp/ovpn/ovpn_server.py
@@ -1,7 +1,9 @@
-from flask import render_template, flash, request
+
+from flask import redirect, render_template, flash, request, url_for
 from flask_login import login_required
-from flaskapp.ovpn.ovpn_server_lib import copy_certs, get_server_status, read_server_conf
-from flaskapp import app
+from flaskapp.models.ovpn_models import Server_settings
+from flaskapp.ovpn.ovpn_server_lib import change_server_status, copy_certs, get_server_logs, get_server_status, read_server_conf, save_server_conf
+from flaskapp import app, db
 
 @app.route('/ovpn_server', methods = ['GET', 'POST'])
 @login_required
@@ -15,8 +17,55 @@ def ovpn_server():
 
 # POST
     if  request.method == 'POST':
-# install openvpn button:
+# saving config:
+        if request.form['button_pressed'] == 'save_config':
+            error = save_server_conf(request.form['conf_string'],1)
+            if error != 'NONE':
+                flash(error, 'danger')
+            return redirect (url_for('ovpn_server'))
+        if (request.form['button_pressed'] == 'start') or (request.form['button_pressed'] == 'stop') or (request.form['button_pressed'] == 'restart'):
+            error = change_server_status(request.form['button_pressed'])
+            return redirect (url_for('ovpn_server'))
         if request.form['button_pressed'] == 'copy_certs':
             error = copy_certs()
-
+            if error != 'NONE':
+                flash (error, 'danger')
+            return redirect(url_for('ovpn_server'))
     return render_template('/ovpn/ovpn_server.html' , ovpn_status = ovpn_status , config_file = config_file)
+
+
+@app.route('/ovpn_server_logs', methods = ['GET', 'POST'])
+@login_required
+def ovpn_server_logs():
+    log, status = get_server_logs()
+
+    return render_template('/ovpn/ovpn_server_logs.html' , log = log , status = status)
+
+# left this idea to other time
+# do not use it now
+@app.route('/ovpn_server/setup', methods = ['GET', 'POST'])
+@login_required
+def ovpn_server_setup():
+    all_settings = Server_settings.query.all()
+    if request.method == 'POST':
+        button_pressed = request.form['button_pressed']
+        if button_pressed == 'add':
+            settings = Server_settings()
+            settings.name = request.form['set_name']
+            settings.value = request.form['set_value']
+            settings.comment = request.form['set_comment']
+            db.session.add(settings)
+            db.session.commit()
+            return redirect(url_for('ovpn_server_setup'))
+#        elif button_pressed.find('edit_') != -1:
+            # Edit button was pressed
+#            set_name = button_pressed[button_pressed.find('edit_')+5:]
+#            set_row = Server_settings.query.filter_by(name = set_name).first()
+        elif button_pressed.find('delete_') != -1:
+            # delete was pressed
+            set_name = button_pressed[button_pressed.find('delete_')+7:]
+            set_row = Server_settings.query.filter_by(name = set_name).first()
+            db.session.delete(set_row)
+            db.session.commit()
+            return redirect(url_for('ovpn_server_setup'))
+    return render_template('/ovpn/server_db_setup.html', all_settings = all_settings)
--- flaskapp/ovpn/ovpn_server_lib.py
+++ flaskapp/ovpn/ovpn_server_lib.py
@@ -5,9 +5,11 @@ from flaskapp.models.models import OVPN_INFO
 from flaskapp.sudo.sudo_lib import sudo_timestemp_reset
 from flask_login import current_user
 from flaskapp import app
+from flask import flash
 
 TEMP_DIR = app.root_path + "/temp/"
 
+# nor working
 def copy_certs():
     error  = "NONE"
     if not sudo_timestemp_reset():
@@ -17,23 +19,98 @@ def copy_certs():
     # and make config_string
     error, config_string = read_server_conf()
     if config_string == "_empty_":
-        error = "Error coping or reading server configuration file"
+        error = "Error copying or reading server configuration file"
         return error
-    #1 changing ca line
-    # looking betweeng "ca" and "cert" - it will work only with sefault config file
-    # result will be something - '\nca ca.crt'
-    cert = config_string[config_string.find("\nca"):config_string.find("\ncert")]
-    # looking on current ca certificate in PKI
+    # getting PKI directory
     error, pki_dir = get_pki_dir()
     if error != 'NONE':
         return error
-    new_cert = "\nca " + pki_dir + "/RootCA/CA/ca.cert"
-    # replaceing certificates
-    config_string = config_string.replace(cert,new_cert)
-    #new file 
-    file = open(TEMP_DIR + "server.tmp","w")
-    file.write(config_string)
-    file.close()
+    # setting ca certificate value
+    ca_cert = pki_dir + '/RootCA/CA/ca.cert'
+    error, config_string = change_setting_value(config_string, 'ca', ca_cert)
+    if error != 'NONE':
+        return error
+    # setting server certificate and key
+    server_files = os.popen("ls " + pki_dir + "/server/ | grep cert").read()
+    if server_files == '':
+        return "Could not find server certificate"
+    else:
+        # here we take first certificate in server folders
+        server_file_name = server_files.split()[0].split('.')[0]
+        server_cert = server_file_name + '.cert'
+        server_key = server_file_name + '.key'
+        error, config_string = change_setting_value(config_string, 'cert', pki_dir + '/server/' + server_cert)
+        if error != 'NONE':
+            return error
+        error, config_string = change_setting_value(config_string, 'key', pki_dir + '/server/' +  server_key)
+        if error != 'NONE':
+            return error
+    error = save_server_conf(config_string)
+#    file = open(TEMP_DIR + "server.tmp","w")
+#    file.write(config_string)
+#    file.close()
+    return error
+
+def change_setting_value(config_string, the_setting, the_value=''):
+    error = 'NONE'
+    if config_string == '' or the_setting == '':
+        return 'Empty config string or setting.', config_string
+    # config_string is basically file with settings
+    # function looks and find 'the_settings' in the begining of strings
+    # if it finds "the_value" will be new
+    # 1 making list by splitting string at '\n' 
+    config_list = config_string.split('\n')
+    # 2 finding setting and meking new value
+    found = False
+    index=0
+    for element in config_list:
+        if element.find(the_setting) == 0:
+            config_list[index] = the_setting + ' ' + the_value
+            found = True
+        index += 1
+    if not found:
+        error = 'Coud not find setting: ' + the_setting
+    # remaking string from the list
+    config_string = '\n'.join(config_list)
+    return error, config_string
+# end of not working
+
+def get_server_logs():
+    log  = os.popen("echo " + current_user.sudo_password_encoded
+                    + " | sudo -S cat /var/log/openvpn/openvpn.log").read()
+    status  = os.popen("echo " + current_user.sudo_password_encoded
+                    + " | sudo -S cat /var/log/openvpn/openvpn-status.log").read()
+
+    return log, status
+
+def change_server_status(status='restart',id=1):
+    error  = "NONE"
+    if not sudo_timestemp_reset():
+        error = 'Please check/reset SUDO password'
+        return error
+    OVPN = OVPN_INFO.query.filter_by(id=id).first()
+    file_path = OVPN.server_file
+    os.system("echo " + current_user.sudo_password_encoded 
+              + " | sudo -S systemctl " + status + " openvpn@" 
+              + os.path.splitext(file_path)[0])
+    return error
+
+def save_server_conf(config_string='', id=1):
+    error  = "NONE"
+    if not sudo_timestemp_reset():
+        error = 'Please check/reset SUDO password'
+        return error
+    # saving string into template file:
+    new_file = open(TEMP_DIR + 'server.new', 'w')
+    new_file.write(config_string)
+    new_file.close()
+    # copy new config file to openVPN directory
+    OVPN = OVPN_INFO.query.filter_by(id=id).first()
+    file_path = OVPN.main_dir + OVPN.server_file
+    os.system("echo " + current_user.sudo_password_encoded 
+              + " | sudo -S cp " 
+              + TEMP_DIR + 'server.new '
+              + file_path)
     return error
 
 def read_server_conf(id=1):
@@ -44,7 +121,7 @@ def read_server_conf(id=1):
         return error, config_file
     OVPN = OVPN_INFO.query.filter_by(id=id).first()
     from_path = OVPN.main_dir + OVPN.server_file
-    to_path = TEMP_DIR + OVPN.server_file[1:]
+    to_path = TEMP_DIR + OVPN.server_file
     #1 copy server config file to temp directory
     # with will help to open this file to write without sudo permission
     # and cope command will be without sudo to rewrite permissions on the file
@@ -65,11 +142,14 @@ def get_server_status(id=1):
     # as example lets server_file = '/server.conf'
     # os.path.splitext(server_file) returns list :('/server', '.conf')
     # we take it first item which is [0] - it is string now
-    # and cut this string from second letter  to last letter in it by [1:] - count starts from 0
-    server_name = os.path.splitext(server_file)[0][1:]
+    server_name = os.path.splitext(server_file)[0]
     os.system("echo " + current_user.sudo_password_encoded 
               + " | sudo -S systemctl status openvpn@" 
               + server_name 
               + " > " + TEMP_DIR + "ovpn_server.status")
+    os.system("echo '#---- netstat ----#' >> "+ TEMP_DIR + "ovpn_server.status")
+    os.system("echo " + current_user.sudo_password_encoded 
+              + " | sudo -S netstat -tuapn | grep openvpn"
+              + " >> " + TEMP_DIR + "ovpn_server.status")    
     server_status = open(TEMP_DIR + "ovpn_server.status").read()
     return error, server_status
